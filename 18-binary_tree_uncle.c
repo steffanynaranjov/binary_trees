d8c61b98b220c112bf37a105357c73afef1f841e
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-26 16:04:02 +0000

    uncle binary tree

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
+/**
+ * binary_tree_uncle - measures the height of a binary tree
+ * @tree: Binary tree
+ * Description: measures the height of a binary tree
+ * Return: Nothing
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
